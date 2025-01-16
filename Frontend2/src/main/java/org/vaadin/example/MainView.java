ac738a2249a84059376e051fea08570eaef1c9cb
Enrique Villar <9202686@alumnos.ufv.es>
2025-01-16 12:00:10 +0100

    Add DataService and Vehicle model to manage vehicle data and display in MainView

--- Frontend2/src/main/java/org/vaadin/example/MainView.java
+++ Frontend2/src/main/java/org/vaadin/example/MainView.java
@@ -7,7 +7,13 @@ import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import com.vaadin.flow.component.grid.Grid;
 
 /**
  * A sample Vaadin view class.
@@ -32,29 +38,19 @@ public class MainView extends VerticalLayout {
      * @param service
      *            The message service. Automatically injected Spring managed bean.
      */
-    public MainView(@Autowired GreetService service) {
+    public MainView(@Autowired GreetService service) throws IOException, InterruptedException, URISyntaxException {
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addClassName("bordered");
+        Grid<Vehicle> grid = new Grid<>(Vehicle.class, false);
+        grid.addColumn(Vehicle::getLicensePlate).setHeader("License Plate");
+        grid.addColumn(Vehicle::getMake).setHeader("Make");
+        grid.addColumn(Vehicle::getModel).setHeader("Model");
+        grid.addColumn(Vehicle::getYear).setHeader("Year");
+        grid.addColumn(Vehicle::getType).setHeader("Type");
+        grid.addColumn(Vehicle::getAvailability).setHeader("Availability");
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello", e -> {
-            add(new Paragraph(service.greet(textField.getValue())));
-        });
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in
-        // styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        ArrayList<Vehicle> vehicle = DataService.getVehicles();
+        grid.setItems(vehicle);
+        
+        add(grid);
     }
 }
